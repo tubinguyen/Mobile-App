06c21751b4cc4b5a3a771480f5caa2beeeab93b3
dynnyd <duongyennhi270904@gmail.com>
2025-04-01 22:24:35 +0700

    add filter

--- app_tienganh/lib/views/home_screen.dart
+++ app_tienganh/lib/views/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../widgets/google_button.dart';
+// import '../widgets/google_button.dart';
+import '../widgets/filter.dart';
 class HomeScreen extends StatelessWidget {
   final Function(int) onNavigate;
 
@@ -12,16 +13,25 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            const Text(
-              'Nội dung Trang chủ',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20), 
-            GoogleSignInButton(
-              onTap: () {
-                onNavigate(4); 
+            // const Text(
+            //   'Nội dung Trang chủ',
+            //   style: TextStyle(fontSize: 24),
+            // ),
+            // const SizedBox(height: 20), 
+            // GoogleSignInButton(
+            //   onTap: () {
+            //     onNavigate(4); 
+            //   },
+            // ),
+            // const SizedBox(height: 20),
+            Filter(
+              options: ['Lọc theo Ngày', 'Lọc theo Tháng', 'Lọc theo Năm'],
+              onSelected: (String value) {
+                // Handle filter selection
+                print('Selected filter: $value');
               },
-            )
+            ),
+            
           ],
         ),
       ),
