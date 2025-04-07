97125617ee31f7f18a93c2d05f515008ac9aa26b
dynnyd <duongyennhi270904@gmail.com>
2025-04-07 17:27:50 +0700

    home screen

--- app_tienganh/lib/views/home_screen.dart
+++ app_tienganh/lib/views/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import '../widgets/google_button.dart';
-import '../widgets/filter.dart';
+import '../widgets/google_button.dart';
 class HomeScreen extends StatelessWidget {
   final Function(int) onNavigate;
 
@@ -13,25 +12,16 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            // const Text(
-            //   'Nội dung Trang chủ',
-            //   style: TextStyle(fontSize: 24),
-            // ),
-            // const SizedBox(height: 20), 
-            // GoogleSignInButton(
-            //   onTap: () {
-            //     onNavigate(4); 
-            //   },
-            // ),
-            // const SizedBox(height: 20),
-            Filter(
-              options: ['Lọc theo Ngày', 'Lọc theo Tháng', 'Lọc theo Năm'],
-              onSelected: (String value) {
-                // Handle filter selection
-                print('Selected filter: $value');
-              },
+            const Text(
+              'Nội dung Trang chủ',
+              style: TextStyle(fontSize: 24),
             ),
-            
+            const SizedBox(height: 20), 
+            GoogleSignInButton(
+              onTap: () {
+                onNavigate(4); 
+              },
+            )
           ],
         ),
       ),
