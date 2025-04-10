57e245182017d86595b22cc97beb2f86e4c6631d
ynhii1905 <22521040@gm.uit.edu.vn>
2025-04-10 09:01:58 +0700

    update course_page

--- app_tienganh/lib/views/cus/course_creation_screen.dart
+++ app_tienganh/lib/views/cus/course_creation_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import '../../widgets/top_app_bar.dart';
+import '../../widgets/header.dart';
+import '../../widgets/navbar.dart';
 import '../../widgets/plus_button.dart';
+import '../../widgets/content_switch.dart';
+import '../../widgets/notification.dart';
 
 class CourseCreationScreen extends StatelessWidget {
 final Function(int) onNavigate;
@@ -9,19 +12,53 @@ final Function(int) onNavigate;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        title: "Tạo Học Phần",
-        onItemTapped: (value) {
-          onNavigate(value);
-        },
+      appBar: AppBar(
+        title: Header(
+          onHomeTap: () {
+            onNavigate(0); // Navigate to home screen
+          },
+          onNotificationTap: () {
+            onNavigate(1); // Navigate to notification screen
+          },
+          onAuthTap: () {
+            onNavigate(2); // Navigate to auth screen
+          },
+        ),
       ),
-      body: Stack(
-        children: [
-          PlusButton(
-            onPressed: () {
-            },
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min, 
+          children: [ CustomNavBar(
+              title: "Thư viện của bạn",
+              leadingIconPath: "assets/img/back.svg",
+              actionIconPath: "assets/img/store.svg",
+              onLeadingPressed: () {
+                Navigator.pop(context);
+              },
+              onActionPressed: () {
+                onNavigate(3); 
+              },
+            ),
+
+            ContentSwitcher(
+              onNavigate: (index) {
+                onNavigate(index); 
+              },
+
+              
+            ),
+
+            SizedBox(height: 106),
+
+            NotificationCard(
+              mainText: 'Bạn chưa có học phần nào',
+              subText: 'các học phần bạn tạo sẽ được lưu tại đây',
+              timeAgo: '9m',
+              svgPath: 'assets/img/book.png',
+              margin: EdgeInsets.symmetric(horizontal: 0, vertical: 4),
+            ),
+          ],
+        ),
       ),
     );
   }
