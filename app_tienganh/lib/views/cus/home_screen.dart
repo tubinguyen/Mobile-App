b15cd14033191f105e014dde23018e750054d7f3
dynnyd <duongyennhi270904@gmail.com>
2025-04-26 15:07:51 +0700

    fixx

--- app_tienganh/lib/views/cus/home_screen.dart
+++ app_tienganh/lib/views/cus/home_screen.dart
@@ -1,33 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:app_tienganh/widgets/book_list.dart';
-
-// class HomeScreen extends StatelessWidget {
-//   final Function(int) onNavigate;
-
-//   const HomeScreen({super.key, required this.onNavigate});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Trang chủ')),
-//       body: Center(
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           children: [
-//             const Text(
-//               'Nội dung Trang chủ',
-//               style: TextStyle(fontSize: 24),
-//             ),
-//             const SizedBox(height: 20),
-//             BookList(), // Gọi BookList đúng cách
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
 import 'package:app_tienganh/widgets/banner.dart';
 import 'package:app_tienganh/widgets/recent_activity.dart';
 import 'package:flutter/material.dart';
